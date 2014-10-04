4f7bd6e763b641e8a21d2486265a918ab92ca509
Irimescu Doru <t3irdo00@students.oamk.fi>
2014-10-04 18:43:24 +0300

    easy printf hack for arduino users

---
+++ print_f.h
@@ -0,0 +1,29 @@
+/*
+  OhmMeter.h v.1.0 - Resistance measuring library for arduino
+  2014 Doru Irimescu 
+
+*/
+
+// ensure this library description is only included once
+#ifndef print_f_h
+#define print_f_h
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+#include <stdarg.h>
+
+// library interface description
+
+void print_f(const char* format, ... )
+{
+  char buffer[240];
+  va_list list;
+  va_start (list, format);
+  vsprintf(buffer,format, list );
+  va_end(list);
+  Serial.print(buffer);
+}
+#endif
